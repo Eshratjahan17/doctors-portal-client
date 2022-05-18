ba21c10553a16d47c35d2c636863841761f38a7a
Eshratjahan17 <eshratjahan701@gmail.com>
2022-05-18 20:20:02 +0600

    1st commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Pages/Home/About/About';
+import Home from './Pages/Home/Home/Home';
+import LogIn from './Pages/Home/Log in/LogIn';
+import Reviwes from './Pages/Home/Reviwes/Reviwes';
+import NotFound from './Pages/NotFound/NotFound';
+import Footer from './Pages/Shared/Footer/Footer';
+import Header from './Pages/Shared/Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About></About>} />
+        <Route path="/reviwes" element={<Reviwes/>} />
+        <Route path="/login" element={<LogIn/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Home/Appointment/Appointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appointment = () => {
+  return (
+    <div>
+      appoinment page
+    </div>
+  );
+};
+
+export default Appointment;
